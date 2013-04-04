c1b530e990cd902a9ec45c021a02908793bb3e8e
Ryan Manalo <ryan.manalo@alum.berkeley.edu>
2013-04-04 14:01:23 -0700

    Made product ids links to product page.

--- store.rb
+++ store.rb
@@ -4,6 +4,12 @@ gem 'sinatra', '1.3.0'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sqlite3'
+require 'better_errors'
+
+configure :development do
+  use BetterErrors::Middleware
+  BetterErrors.application_root = File.expand_path("..", __FILE__)
+end
  
  
 get '/users' do
